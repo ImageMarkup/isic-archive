72958816639045bfef8e62de5306b671dd2c3fb9
Brian Helba <brian.helba@kitware.com>
2015-01-21 06:21:04 -0500

    Greatly simplify static HTML routes

--- server/__init__.py
+++ server/__init__.py
@@ -1,16 +1,18 @@
+# coding=utf-8
 
 import functools
 import os
 
+import cherrypy
 from girder import events
-from provision_utility import initialSetup
-from gallery import GalleryHandler
-from upload import uploadHandler
-from image_utility import zoomifyhandler, thumbnailhandler, fifHandler, annotationHandler, segmentationSourceHandler, segmentationTileHandler
-from qc import QCHandler
-from task_utility import tasklisthandler, TaskHandler, UDAResource
+from girder.utility.server import _StaticFileRoute
 
-from annotate import AnnotateHandler, FillHandler, MapHandler
+from .annotate import FillHandler
+from .image_utility import zoomifyhandler, thumbnailhandler, fifHandler, annotationHandler, segmentationSourceHandler, segmentationTileHandler
+from .provision_utility import initialSetup
+from .task_utility import tasklisthandler, TaskHandler, UDAResource
+from .upload import uploadHandler
+
 
 def public_access(fun):
     @functools.wraps(fun)
@@ -20,15 +22,22 @@ def public_access(fun):
     return accessDecorator
 
 
+class StaticRouteWithId(_StaticFileRoute):
+    """
+    This creates a static route with an optional nonfunctional "/:id" variable
+    path component at the end.
+    """
+    @cherrypy.popargs('id')
+    def GET(self, id=None):
+        return super(StaticRouteWithId, self).GET()
+
+
 def load(info):
-
-
     # create all necessary users, groups, collections, etc
     initialSetup()
 
 
     # add static file serving
-
     app_base = os.path.join(os.curdir, os.pardir)
     app_path = os.path.join(app_base, 'girder', 'plugins', 'uda', 'custom')
 
@@ -40,93 +49,57 @@ def load(info):
 
     # add dynamic root routes
     # root endpoints -> where a user may go and expect a UI
-
     class Root(object):
         pass
+    info['serverRoot'].uda = Root()
 
-    uda_root = Root()
+    # "/uda/gallery/:folderId" -> returns a single page gallery
+    info['serverRoot'].uda.gallery = StaticRouteWithId(os.path.join(info['pluginRootDir'], 'custom', 'gallery.html'))
 
+    # "uda/qc/:folderId" -> returns a QC page where user can move images to
+    info['serverRoot'].uda.qc = StaticRouteWithId(os.path.join(info['pluginRootDir'], 'custom', 'qc.html'))
 
-    info['apiRoot'].uda = UDAResource()
-
-    #   uda/gallery/:folderId -> returns a single page gallery
-
-    uda_root.gallery = GalleryHandler()
-
-
-
-    #   uda/qc/:folderId -> returns a QC page where user can move images to
-
-    uda_root.qc = QCHandler()
-
-
-
-    #   uda/view/:itemId -> simple zoomable viewer for an image
-
+    # "uda/view/:itemId" -> simple zoomable viewer for an image
     # TODO
 
+    # "/uda/task/:userId" -> redirects to appropriate task view for the user
+    info['serverRoot'].uda.task = TaskHandler()
 
+    # "/uda/annotator/:id" -> the reconfigurable image annotator
+    info['serverRoot'].uda.annotate = StaticRouteWithId(os.path.join(info['pluginRootDir'], 'custom', 'annotate.html'))
 
-    #   uda/task/:userId -> redirects to appropriate task view for the user
+    # "/uda/map/:id"
+    info['serverRoot'].uda.map = StaticRouteWithId(os.path.join(info['pluginRootDir'], 'custom', 'map.html'))
 
-    uda_root.task = TaskHandler()
-
-
-
-    #   uda/annotator -> the reconfigurable image annotator
-
-    uda_root.annotate = AnnotateHandler()
-
-    uda_root.map = MapHandler()
-
-    uda_root.fill = FillHandler()
-
-
-    # add route to root route '/'
-
-    info['serverRoot'].uda = uda_root
-
-
-
+    # "/uda/fill/:id" (POST only)
+    info['serverRoot'].uda.fill = FillHandler()
 
 
     # add api routes
+    info['apiRoot'].uda = UDAResource()
 
-    # item/:id/thumbnail -> returns a thumbnail of the image
-
+    # "/api/v1/item/:id/thumbnail" -> returns a thumbnail of the image
     info['apiRoot'].item.route('GET', (':id', 'thumbnail'), public_access(thumbnailhandler))
 
-    # item/:id/annotation -> returns the json annotation
-
+    # "/api/v1/item/:id/annotation" -> returns the json annotation
     info['apiRoot'].item.route('GET', (':id', 'annotation'), public_access(annotationHandler))
 
-
-    # item/:id/annotation -> returns the png segmentation (index map as alpha channel)
-
+    # "/api/v1/item/:id/segmentationSource" -> returns the png segmentation (index map as alpha channel)
     info['apiRoot'].item.route('GET', (':id', 'segmentationSource'), public_access(segmentationSourceHandler))
-
+    # "/api/v1/item/:id/segmentationTiles"
     info['apiRoot'].item.route('GET', (':id', 'segmentationTiles'), public_access(segmentationTileHandler))
 
-
-    # item/:id/zoomify -> returns a zoomify xml if available
-
+    # "/api/v1/item/:id/zoomify/:p1" -> returns a zoomify xml if available
     info['apiRoot'].item.route('GET', (':id', 'zoomify', ':p1'), public_access(zoomifyhandler))
+    # "/api/v1/item/:id/zoomify/:p1/:p2"
     info['apiRoot'].item.route('GET', (':id', 'zoomify', ':p1', ':p2'), public_access(zoomifyhandler))
 
-
-    # item/:id/fif -> returns the IIP FIF endpoint for an item
-
+    # "/api/v1/item/:id/fif/:fifparams" -> returns the IIP FIF endpoint for an item
     info['apiRoot'].item.route('GET', (':id', 'fif', ':fifparams'), public_access(fifHandler))
 
-    # user/:userId/tasklist -> returns a list of images and any UI configuration
-
+    # "/api/v1/user/:userId/tasklist" -> returns a list of images and any UI configuration
     info['apiRoot'].user.route('GET', (':id', 'tasklist'), public_access(tasklisthandler))
 
+
     # add event listeners
-
     events.bind('data.process', 'uploadHandler', public_access(uploadHandler))
-
-
-
-
-    # add the base directory to serve
--- server/annotate.py
+++ server/annotate.py
@@ -1,119 +1,20 @@
-__author__ = 'stonerri'
+# coding=utf-8
 
+import json
 
-from girder.utility.model_importer import ModelImporter
-from girder.api.describe import Description
 import cherrypy
-import os
-import datetime
-from model_utility import *
 
-from image_processing import *
-
-
-class AnnotateHandler(object):
-    exposed = True
-    def __init__(self):
-        pass
-
-    # this line will map the first argument after / to the 'id' parameter
-    # for example, a GET request to the url:
-    # http://localhost:8000/items/
-    # will call GET with id=None
-    # and a GET request like this one: http://localhost:8000/items/1
-    # will call GET with id=1
-    # you can map several arguments using:
-    # @cherrypy.propargs('arg1', 'arg2', 'argn')
-    # def GET(self, arg1, arg2, argn)
-
-    @cherrypy.popargs('id')
-    def GET(self, id=None):
-
-        # app_base = os.path.join(os.curdir, os.pardir)
-        # qc_app_path = os.path.join(app_base, 'uda')
-        app_base = os.path.join(os.curdir, os.pardir)
-        app_path = os.path.join(app_base, 'girder', 'plugins', 'uda', 'custom')
-
-        gallery_html = os.path.abspath(os.path.join(app_path, u'annotate.html'))
-
-        fid = open(gallery_html, 'r')
-        gallery_content = fid.read()
-        fid.close()
-
-        return gallery_content
-
-
-
-
-    # HTML5
-    def OPTIONS(self):
-        cherrypy.response.headers['Access-Control-Allow-Credentials'] = True
-        cherrypy.response.headers['Access-Control-Allow-Origin'] = cherrypy.request.headers['ORIGIN']
-        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'POST'
-        cherrypy.response.headers['Access-Control-Allow-Headers'] = cherrypy.request.headers['ACCESS-CONTROL-REQUEST-HEADERS']
-
-
-
-
-class MapHandler(object):
-    exposed = True
-    def __init__(self):
-        pass
-
-    # this line will map the first argument after / to the 'id' parameter
-    # for example, a GET request to the url:
-    # http://localhost:8000/items/
-    # will call GET with id=None
-    # and a GET request like this one: http://localhost:8000/items/1
-    # will call GET with id=1
-    # you can map several arguments using:
-    # @cherrypy.propargs('arg1', 'arg2', 'argn')
-    # def GET(self, arg1, arg2, argn)
-
-    @cherrypy.popargs('id')
-    def GET(self, id=None):
-
-        # app_base = os.path.join(os.curdir, os.pardir)
-        # qc_app_path = os.path.join(app_base, 'uda')
-        app_base = os.path.join(os.curdir, os.pardir)
-        app_path = os.path.join(app_base, 'girder', 'plugins', 'uda', 'custom')
-
-        gallery_html = os.path.abspath(os.path.join(app_path, u'map.html'))
-
-        fid = open(gallery_html, 'r')
-        gallery_content = fid.read()
-        fid.close()
-
-        return gallery_content
-
-
-
-
-    # HTML5
-    def OPTIONS(self):
-        cherrypy.response.headers['Access-Control-Allow-Credentials'] = True
-        cherrypy.response.headers['Access-Control-Allow-Origin'] = cherrypy.request.headers['ORIGIN']
-        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'POST'
-        cherrypy.response.headers['Access-Control-Allow-Headers'] = cherrypy.request.headers['ACCESS-CONTROL-REQUEST-HEADERS']
-
-
-
-
-
+from .image_processing import fillImageGeoJSON, segmentImage
 
 
 class FillHandler(object):
     exposed = True
-    def __init__(self):
-        pass
 
     @cherrypy.popargs('id')
     def POST(self, id=None):
-
         post_body = cherrypy.request.body.read()
-
-        import json
         params = json.loads(post_body)
+
         results = fillImageGeoJSON(params)
 
         return json.dumps(results)
@@ -121,18 +22,12 @@ class FillHandler(object):
 
 class SegmentationHandler(object):
     exposed = True
-    def __init__(self):
-        pass
 
     @cherrypy.popargs('id')
     def POST(self, id=None):
-
         post_body = cherrypy.request.body.read()
-
-        import json
         params = json.loads(post_body)
+
         results = segmentImage(params)
 
         return json.dumps(results)
-
-
