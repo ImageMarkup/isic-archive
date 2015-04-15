056602b7c3238ffd08566c09effcac37a40cde91
Brian Helba <brian.helba@kitware.com>
2015-04-15 08:57:30 -0400

    Add task list link to GlobalNavView

--- web_client/js/taskaction.js
+++ web_client/js/taskaction.js
@@ -13,8 +13,16 @@ girder.wrap(girder.views.LayoutHeaderUserView, 'render', function (render) {
 });
 
 
-girder.views.LayoutHeaderUserView.prototype.events['click a.a-mytasks-link'] = function () {
+girder.wrap(girder.views.LayoutGlobalNavView, 'render', function (render) {
+    render.call(this);
 
+    if (girder.currentUser) {
+        this.$('ul.g-global-nav').prepend('<li class="g-global-nav-li" style="background-color: #7edba9"><a href="/uda/task" g-name="Collections" class="g-nav-link"><i class="icon-picture"></i><span>Image Tasks</span></a></li>');
+    }
+});
+
+
+girder.views.LayoutHeaderUserView.prototype.events['click a.a-mytasks-link'] = function () {
     window.open(
       '/uda/task',
       '_blank'
