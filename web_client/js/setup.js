34d3b69be56b7032c1a3b7a487d678a751061332
Brian Helba <brian.helba@kitware.com>
2015-10-09 07:12:07 -0400

    Add nav buttons for Lesion Images and Annotation Studies

--- web_client/js/setup.js
+++ web_client/js/setup.js
@@ -4,16 +4,55 @@
  * Add a link to the Task Dashboard on the main Girder nav panel.
  */
 girder.wrap(girder.views.LayoutGlobalNavView, 'render', function (render) {
-    if (girder.currentUser) {
-        this.defaultNavItems.unshift({
-            name: 'Image Tasks',
+    'use strict';
+
+    var isicNavItems = [
+        {
+            name: 'Lesion Images',
             icon: 'icon-picture',
+            target: 'isic-images'
+        },
+        {
+            name: 'Annotation Studies',
+            icon: 'icon-eye',
+            target: 'isic-studies'
+        }
+    ];
+    if (girder.currentUser) {
+        isicNavItems.push({
+            name: 'Your Tasks',
+            icon: 'icon-check',
             target: 'isic-tasks'
         });
-    'use strict';
     }
+    this.navItems = isicNavItems.concat(this.defaultNavItems);
 
     render.call(this);
+});
+
+function _navigateToCollection(collectionName, replace) {
+    girder.restRequest({
+        path: '/resource/lookup',
+        data: {'path': '/collection/' + collectionName},
+        type: 'GET'
+    }).done(function (response) {
+        var imagesCollectionId = response._id;
+        // TODO: handle missing collection
+        girder.router.navigate('collection/' + imagesCollectionId, {
+            trigger: true,
+            replace: replace
+        });
+    });
+}
+
+girder.router.route('isic-images', 'isic-images', function () {
+    'use strict';
+    _navigateToCollection('Lesion Images', true);
+});
+
+girder.router.route('isic-studies', 'isic-studies', function () {
+    'use strict';
+    _navigateToCollection('Annotation Studies', true);
 });
 
 girder.router.route('isic-tasks', 'isic-tasks', function () {
