8784dfeb8d629a7a491ba970c3de1da37e6e487c
David Gutman <dagutman@gmail.com>
2016-03-30 17:18:23 -0400

    adding svg colorization

--- custom/isic_challenge_viewer.js
+++ custom/isic_challenge_viewer.js
@@ -7,55 +7,14 @@ function random(range) {
 var my_points; //Making this global for debugging
 
 
-// function contourdata_to_shape(contours, img_width) {
-//     scale_factor = 1;
-//     polygon_list = [];
 
-//     //Openseadragon uses the image width for bo the x and y scale factor... probably should rename this pixel factor
-//     x_scale_factor = 1.0 / img_width;
-//     y_scale_factor = 1.0 / img_width;
-
-//     x_scale_factor = 1;
-//     y_scale_factor = 1;
-
-
-//     $.each(contours, function(index, contour) {
-//         coord_info = contour.geometry.coordinates;
-
-//         coord_string = "";
-//         $.each(coord_info, function(k, v) {
-//             coord_string += `${(v[0]* x_scale_factor ) },${ ( v[1] * y_scale_factor) } `;
-//         }); // the |0 made them all integers
-
-//         //polygon_svg_str = `<polygon points="${coord_string}" style="fill: ${colours[ random(9)]}; stroke; purple; stroke-width: 1; opacity: 0.5;" id="tile${contour.properties.labelindex}" class="tileClass" />`;
-//         labelindex = contour.properties.labelindex;
-
-//         polygon_list.push({
-//             'coords': coord_string,
-//             'labelindex': contour.properties.labelindex
-//         });
-//     });
-
-//     //Need to add the below function to a callback function for above..
-//     $(".tileClass").hover(function() {
-//         console.log(this.id)
-//     });
-//     return polygon_list;
-// }
-
-
-
-var colours = ['purple', 'blue', 'green  ', 'navy', 'green', 'navy', 'blue', 'pink', 'orange', 'yellow', 'lime', 'green', 'blue', 'navy', 'black'];
-
-function loadSVGTileData(svg_json) {
-    console.log('need to load svg data for' + svg_json);
-    // SVG_file = image_info_list[imageName].superpixel_svg;
-    // img_height = image_info_list[imageName].img_height;
-    // img_width = image_info_list[imageName].img_width;
-    img_height = 1000
-    img_width = 1000
-    SVG_file = svg_json
-
+function loadSVGTileData(image_info) {
+    
+    
+    SVG_file = image_info.svgjson_file;
+    img_height = image_info.image_height;
+    img_width = image_info.image_width;
+    /* TO DO:  Fix this ugly mess of a load function */
     new_geodata = function() {
         console.log('loading new geo data');
         geo_array = [];
@@ -77,93 +36,122 @@ function loadSVGTileData(svg_json) {
 
     //  This will iterate through all the tiles and color them a different color to show the tile overlays
     $(".tileClass").remove();
-    //my_points = contourdata_to_shape(new_geodata, img_width);
+    
     //This generates the pretty multicolor tile image
-	//	console.log(my_points);
+    // TO DO:  MAY WANT TO READD THE SCALE FACTOR FOR OSD TO THE COORDINATE ARRAY WHICH MEANS I HAVE TO DIVIDE EACH elementsby the iamge width
+
 	my_points = geo_array;
 	//console.log(my_points);
+	d3_20 = d3.scale.category20();  //Build a color palette of 20 colors from d3
     $.each(my_points, function(k, point_list) {
-        col = colours[ random(9)];
-
-        	//my_points[2].geometry.coordinates.join(' ')  I need to flatten the coords into a string
-        //console.log(point_list);
-        d3.select("#p2_svg_target").append("polygon").attr("points", point_list.geometry.coordinates ).style('fill', col).attr('opacity', 0.9).attr('class', 'tileClass').attr('id', 'tile' + point_list.properties.labelindex);
+        col = d3_20( random(20)); //Pick a color at random
+        d3.select("#p2_svg_target").append("polygon").attr("points", point_list.geometry.coordinates ).style('fill', col).attr('opacity', 0.2).attr('class', 'tileClass').attr('id', 'tile' + point_list.properties.labelindex);
     });
+
+    //I also need to modify the viewBox of the SVG element based on the image width and height of the current image
+	$("#p2_svg_target").removeAttr("viewBox")
+    new_viewbox = "0 0 " + img_width + ' ' + img_height;
+    d3.select("#p2_svg_target").attr("viewBox",new_viewbox)
+    //So this is a bug/feature of SVG and jquery, it inserts the attributes as viewbox NOT viewBox (aka see the spelling)
+    //So I am using d3 and not jQuery to modifiy this attributes
+
+    //ALSO NEED TO CHANGE AND UPDATE THE BACKGROUND IMAGE FOR THE SVG
+    $("#p2_baseimgsvg").attr('xlink:href',image_info.image_path);
+    //Also need to update the width and height of the image container
+    $("#p2_baseimgsvg").attr('height',image_info.image_height);
+    $("#p2_baseimgsvg").attr('width',image_info.image_width);
+
 }
 
 
+function show_features(image_id, feature_to_display)
+	{
+		//Given an image_id, this will display the superpixels that have been marked up for that image
+
+		console.log('need features for'+image_id,feature_to_display);
+		$(".tileClass").attr('opacity',0);
+
+		//This is a lame hack, rr gets updated with the latest image I am looking at
+		sup_pix_for_feat = rr[feature_to_display];
+		//console.log(sup_pix_for_feat);
+		$.each(sup_pix_for_feat,function(k,v) { if (v!=0){ $("#tile"+k).attr('opacity',0.8);$("#tile"+k).css('fill','blue') }
+	}
 
 
+			)
 
-function new_mark_superpixels(sp_info) {
-    $(".tileClass").css('fill', 'none');
-
-    //need to add in the current_feature
-    if (superpixel_markup_info[current_feature]) {
-        num_superpixels = Object.keys(superpixel_markup_info[current_feature]).length;
-    } else {
-        return;
-    }
-
-    // console.log(num_superpixels)
-    $("#tile_info_stats").empty(); /// clear the current DIV before I start putting stats in it..
-    //        $("#tile_info_stats").append(num_superpixels.toString() + ' superpixels are in this image');
-    //This code is very sensitive to on load events...
-    $.each(superpixel_markup_info[current_feature], function(tileID, rater_data) {
-        //         ///I am now iteration over each tile in the selected Image...
-        tileAnnotated = false;
-        raters_for_tile = [];
-
-        //So because of the way we store the data, I need to iterate through each superpixel and determine which rater(s) marked it up.. then color things
-        $.each(rater_data, function(r, v) {
-            if (v != "0.0") {
-                //                        console.log(r,v,tileID);
-                tileAnnotated = true;
-                raters_for_tile.push(r);
-            }
-        });
-
-        if (tileAnnotated) {
-            //Now that I know the tile has been annotated, I need to render the results
-            //console.log(raters_for_tile, tileID);
-
-            if (raters_for_tile.length == 1) {
-                //In this case the tile is colored for the individual rater.... otherwise it's a color palette
-                //Currently I am going
-                //var a = fruits.indexOf("Apple");
-                rater_index = raters.indexOf(raters_for_tile[0]);
-                // console.log(raters_for_tile,rater_index)
-
-                $("#tile" + tileID).css('fill', colours[rater_index]);
-
-            } else {
-                $("#tile" + tileID).css('fill', color_heatmap[raters_for_tile.length]);
-            }
-        }
-
-    });
-
-    $(".tileClass").hover(function() {
-        //  console.log(this.id);
-        pixnum = (this.id).substring(4);
-        $("#tile_info_stats").empty(); /// clear the current DIV before I start putting stats in it..
-        //              $("#tile_info_stats").append(num_superpixels.toString() + ' superpixels are in this image');
-        $("#tile_info_stats").append("<br>You are hovering on sp: " + this.id);
+	}
 
 
-        tile_num = this.id.substring(4); //need to turn the tileID which actually is tile234 or similar into only the number part.. so skip the first 4 chars
-        //Determine who marked up this file
-        pix_raters = superpixel_markup_info[current_feature][tile_num];
-        //remember this may contain ALL raters who assessed this feature, so need to make sure there's not a "0.0" there which means that rater
-        //didn't actually mark that specific superpixel with the given feature
-        rft = [];
-        $.each(pix_raters, function(k, v) {
-            if (v != '0.0') {
-                rft.push(k);
-            }
-        });
-        //console.log(rft);
-        $("#tile_info_stats").append("<br>Raters: " + JSON.stringify(rft));
-    });
+// function new_mark_superpixels(sp_info) {
+//     $(".tileClass").css('fill', 'none');
 
-}
+//     //need to add in the current_feature
+//     if (superpixel_markup_info[current_feature]) {
+//         num_superpixels = Object.keys(superpixel_markup_info[current_feature]).length;
+//     } else {
+//         return;
+//     }
+
+//     // console.log(num_superpixels)
+//     $("#tile_info_stats").empty(); /// clear the current DIV before I start putting stats in it..
+//     //        $("#tile_info_stats").append(num_superpixels.toString() + ' superpixels are in this image');
+//     //This code is very sensitive to on load events...
+//     $.each(superpixel_markup_info[current_feature], function(tileID, rater_data) {
+//         //         ///I am now iteration over each tile in the selected Image...
+//         tileAnnotated = false;
+//         raters_for_tile = [];
+
+//         //So because of the way we store the data, I need to iterate through each superpixel and determine which rater(s) marked it up.. then color things
+//         $.each(rater_data, function(r, v) {
+//             if (v != "0.0") {
+//                 //                        console.log(r,v,tileID);
+//                 tileAnnotated = true;
+//                 raters_for_tile.push(r);
+//             }
+//         });
+
+//         if (tileAnnotated) {
+//             //Now that I know the tile has been annotated, I need to render the results
+//             //console.log(raters_for_tile, tileID);
+
+//             if (raters_for_tile.length == 1) {
+//                 //In this case the tile is colored for the individual rater.... otherwise it's a color palette
+//                 //Currently I am going
+//                 //var a = fruits.indexOf("Apple");
+//                 rater_index = raters.indexOf(raters_for_tile[0]);
+//                 // console.log(raters_for_tile,rater_index)
+
+//                 $("#tile" + tileID).css('fill', colours[rater_index]);
+
+//             } else {
+//                 $("#tile" + tileID).css('fill', color_heatmap[raters_for_tile.length]);
+//             }
+//         }
+
+//     });
+
+//     $(".tileClass").hover(function() {
+//         //  console.log(this.id);
+//         pixnum = (this.id).substring(4);
+//         $("#tile_info_stats").empty(); /// clear the current DIV before I start putting stats in it..
+//         //              $("#tile_info_stats").append(num_superpixels.toString() + ' superpixels are in this image');
+//         $("#tile_info_stats").append("<br>You are hovering on sp: " + this.id);
+
+
+//         tile_num = this.id.substring(4); //need to turn the tileID which actually is tile234 or similar into only the number part.. so skip the first 4 chars
+//         //Determine who marked up this file
+//         pix_raters = superpixel_markup_info[current_feature][tile_num];
+//         //remember this may contain ALL raters who assessed this feature, so need to make sure there's not a "0.0" there which means that rater
+//         //didn't actually mark that specific superpixel with the given feature
+//         rft = [];
+//         $.each(pix_raters, function(k, v) {
+//             if (v != '0.0') {
+//                 rft.push(k);
+//             }
+//         });
+//         //console.log(rft);
+//         $("#tile_info_stats").append("<br>Raters: " + JSON.stringify(rft));
+//     });
+
+// }
