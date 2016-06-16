9850828f27934864c1b4e7c6d2beae274b3d825f
Max Smolens <max.smolens@kitware.com>
2016-06-16 13:18:13 -0400

    Add client StudyModel method to add user

--- web_external/js/models/StudyModel.js
+++ web_external/js/models/StudyModel.js
@@ -1,3 +1,20 @@
 isic.models.StudyModel = girder.Model.extend({
-    resourceName: 'study'
+    resourceName: 'study',
+
+    /**
+     * Add a user to the study.
+     */
+    addUser: function (userId) {
+        girder.restRequest({
+            path: this.resourceName + '/' + this.get('_id') + '/user',
+            type: 'POST',
+            data: {
+                userId: userId
+            }
+        }).done(_.bind(function (resp) {
+            this.trigger('g:addedUser');
+        }, this)).error(_.bind(function (err) {
+            this.trigger('g:error', err);
+        }, this));
+    }
 });
