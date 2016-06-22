2bdb2f55204c676094e59413097fc8ec61e1b1de
Max Smolens <max.smolens@kitware.com>
2016-06-22 15:29:14 -0400

    Add method to check whether a user is a study administrator

--- web_external/js/models/StudyModel.js
+++ web_external/js/models/StudyModel.js
@@ -16,5 +16,37 @@ isic.models.StudyModel = girder.Model.extend({
         }, this)).error(_.bind(function (err) {
             this.trigger('g:error', err);
         }, this));
+    },
+
+    /**
+     * Check whether user is a a study administrator.
+     *
+     * Returns a promise that is resolved with a single boolean argument that
+     * indicates whether the user is a study administrator.
+     *
+     */
+    isAdministrator: function (user) {
+        var deferred = $.Deferred();
+        if (user) {
+            var groups = new girder.collections.GroupCollection();
+            groups.once('g:changed', function () {
+                if (!groups.isEmpty()) {
+                    var groupId = groups.first().id;
+                    var userGroups = user.get('groups');
+                    var studyAdmin = _.contains(userGroups, groupId);
+                    deferred.resolve(studyAdmin);
+                } else {
+                    deferred.resolve(false);
+                }
+            }, this).once('g:error', function () {
+                deferred.reject();
+            }, this).fetch({
+                text: 'Study Administrators',
+                exact: true
+            });
+        } else {
+            deferred.resolve(false);
+        }
+        return deferred.promise();
     }
 });
