4e05d9886f3fc5ab7ba90058d4b8deeba92ef545
Brian Helba <brian.helba@kitware.com>
2016-06-23 20:22:01 -0400

    Add Python style tests to model classes

--- plugin.cmake
+++ plugin.cmake
@@ -1,6 +1,28 @@
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+add_python_style_test(
+  python_static_analysis_isic_archive_models
+  "${CMAKE_CURRENT_LIST_DIR}/server/models"
+)
+
 add_eslint_test(
-  isic_archive "${PROJECT_SOURCE_DIR}/plugins/isic_archive/web_external/js"
-  ESLINT_CONFIG_FILE "${PROJECT_SOURCE_DIR}/plugins/isic_archive/web_external/js/.eslintrc.js"
-  ESLINT_IGNORE_FILE "${PROJECT_SOURCE_DIR}/plugins/isic_archive/web_external/js/.eslintignore"
+  js_static_analysis_isic_archive_external
+  "${CMAKE_CURRENT_LIST_DIR}/web_external/js"
+  ESLINT_CONFIG_FILE "${CMAKE_CURRENT_LIST_DIR}/web_external/js/.eslintrc.js"
+  ESLINT_IGNORE_FILE "${CMAKE_CURRENT_LIST_DIR}/web_external/js/.eslintignore"
 )
 
