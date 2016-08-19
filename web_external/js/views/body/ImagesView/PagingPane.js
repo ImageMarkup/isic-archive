19e9ceb96063a80464ed5dd99480266dabfb83a2
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-08-19 15:47:15 -0400

    Move the histogram and its toggle button

--- web_external/js/views/body/ImagesView/PagingPane.js
+++ web_external/js/views/body/ImagesView/PagingPane.js
@@ -6,7 +6,6 @@ isic.views.ImagesSubViews = isic.views.ImagesSubViews || {};
 isic.views.ImagesSubViews.PagingPane = Backbone.View.extend({
   initialize: function () {
     var self = this;
-    self.showHistograms = false;
   },
   render: function () {
     var self = this;
@@ -20,19 +19,6 @@ isic.views.ImagesSubViews.PagingPane = Backbone.View.extend({
             imgName + '.svg';
         });
       self.addedImages = true;
-    }
-    if (!self.addedDomListeners) {
-      self.$el.find('#isic-images-toggleHistogram').on('click', function () {
-        self.showHistograms = !self.showHistograms;
-        self.trigger('iv:toggleHistogram');
-      });
-      self.addedDomListeners = true;
-    }
-
-    if (self.showHistograms) {
-      self.$el.find('#isic-images-toggleHistogram').addClass('selected');
-    } else {
-      self.$el.find('#isic-images-toggleHistogram').removeClass('selected');
     }
   }
 });
