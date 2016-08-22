945e387fe4624f220c27218b796946eb1d2e214b
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-08-22 08:25:58 -0400

    Add a close box
    Also give all views a reference to the main view, and fix some broken styles

--- web_external/js/views/body/ImagesView/HistogramPane.js
+++ web_external/js/views/body/ImagesView/HistogramPane.js
@@ -3,8 +3,9 @@
 isic.views.ImagesSubViews = isic.views.ImagesSubViews || {};
 
 isic.views.ImagesSubViews.HistogramPane = Backbone.View.extend({
-    initialize: function () {
-
+    initialize: function (params) {
+        var self = this;
+        self.parentView = params.parentView;
     },
     render: function () {
         return this;
