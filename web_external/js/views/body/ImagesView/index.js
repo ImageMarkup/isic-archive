a97143d5c5177249e6bb0e5bd68ec87c0e042b0e
Brian Helba <brian.helba@kitware.com>
2016-08-22 00:16:13 -0400

    Fix style issues

--- web_external/js/views/body/ImagesView/index.js
+++ web_external/js/views/body/ImagesView/index.js
@@ -1,101 +1,100 @@
-/*globals girder, jQuery, d3, Image*/
-/*eslint-disable*/
+/*globals girder, jQuery, Image, recolorImageFilters*/
 
 isic.views.ImagesSubViews = isic.views.ImagesSubViews || {};
 
 isic.views.ImagesView = isic.View.extend({
-  initialize: function () {
-    var self = this;
-    // Initialize our subviews
-    self.histogramPane = new isic.views.ImagesSubViews.HistogramPane();
-    self.imageWall = new isic.views.ImagesSubViews.ImageWall();
-    self.pagingPane = new isic.views.ImagesSubViews.PagingPane();
-    self.imageDetailsPane = new isic.views.ImagesSubViews.ImageDetailsPane();
+    initialize: function () {
+        var self = this;
+        // Initialize our subviews
+        self.histogramPane = new isic.views.ImagesSubViews.HistogramPane();
+        self.imageWall = new isic.views.ImagesSubViews.ImageWall();
+        self.pagingPane = new isic.views.ImagesSubViews.PagingPane();
+        self.imageDetailsPane = new isic.views.ImagesSubViews.ImageDetailsPane();
 
-    window.onresize = function () {
-      self.render();
-    };
-    self.attachListeners();
-    self.updateCurrentPage();
-  },
-  attachListeners: function () {
-    var self = this;
-    self.listenTo(self.histogramPane, 'iv:changeFilters',
-      function () {
+        window.onresize = function () {
+            self.render();
+        };
+        self.attachListeners();
         self.updateCurrentPage();
-      });
+    },
+    attachListeners: function () {
+        var self = this;
+        self.listenTo(self.histogramPane, 'iv:changeFilters',
+            function () {
+                self.updateCurrentPage();
+            });
 
-    self.listenTo(self.imageWall, 'iv:selectImage',
-      function (imageId) {
-        self.imageDetailsPane.updateDetails(imageId);
-        self.render();
-      });
+        self.listenTo(self.imageWall, 'iv:selectImage',
+            function (imageId) {
+                self.imageDetailsPane.updateDetails(imageId);
+                self.render();
+            });
 
-    self.listenTo(self.pagingPane, 'iv:toggleHistogram',
-      function () {
-        // Regardless of whether this goes on or off,
-        // we want to remove hide the selected item details
-        // (i.e. if we're turning it off, we want to close
-        // the sidebar. If we're turning it on, we want to
-        // show the histograms, not the individual selection)
-        self.imageWall.selectImage(null);
-        self.render();
-      });
-  },
-  updateCurrentPage: function () {
-    var self = this;
-    // TODO: pass in filters and paging settings
-    // var filterString = self.histogramPane.getFilterString();
-    girder.restRequest({
-      path: 'image',
-      data: {
-        'limit': 50,
-        'offset': 0
-      }
-    }).done(function (resp) {
-      var newImageIds = resp.map(function (imageObj) {
-        return imageObj._id;
-      });
-      self.imageWall.setImages(newImageIds);
-      self.render();
-    });
-  },
-  render: function () {
-    var self = this;
-    if (!(self.addedTemplate)) {
-      self.$el.html(isic.templates.imagesPage());
-      recolorImageFilters(['#00ABFF', '#444499']);
-      self.histogramPane.setElement(self.$el.find('#isic-images-histogramPane')[0]);
-      self.histogramPane.addedDomListeners = false;
-      self.imageWall.setElement(self.$el.find('#isic-images-imageWall')[0]);
-      self.imageWall.addedDomListeners = false;
-      self.pagingPane.setElement(self.$el.find('#isic-images-pagingPane')[0]);
-      self.pagingPane.addedDomListeners = false;
-      self.imageDetailsPane.setElement(self.$el.find('#isic-images-imageDetailsPane')[0]);
-      self.imageDetailsPane.addedDomListeners = false;
-      self.addedTemplate = true;
+        self.listenTo(self.pagingPane, 'iv:toggleHistogram',
+            function () {
+                // Regardless of whether this goes on or off,
+                // we want to remove hide the selected item details
+                // (i.e. if we're turning it off, we want to close
+                // the sidebar. If we're turning it on, we want to
+                // show the histograms, not the individual selection)
+                self.imageWall.selectImage(null);
+                self.render();
+            });
+    },
+    updateCurrentPage: function () {
+        var self = this;
+        // TODO: pass in filters and paging settings
+        // var filterString = self.histogramPane.getFilterString();
+        girder.restRequest({
+            path: 'image',
+            data: {
+                'limit': 50,
+                'offset': 0
+            }
+        }).done(function (resp) {
+            var newImageIds = resp.map(function (imageObj) {
+                return imageObj._id;
+            });
+            self.imageWall.setImages(newImageIds);
+            self.render();
+        });
+    },
+    render: function () {
+        var self = this;
+        if (!(self.addedTemplate)) {
+            self.$el.html(isic.templates.imagesPage());
+            recolorImageFilters(['#00ABFF', '#444499']);
+            self.histogramPane.setElement(self.$el.find('#isic-images-histogramPane')[0]);
+            self.histogramPane.addedDomListeners = false;
+            self.imageWall.setElement(self.$el.find('#isic-images-imageWall')[0]);
+            self.imageWall.addedDomListeners = false;
+            self.pagingPane.setElement(self.$el.find('#isic-images-pagingPane')[0]);
+            self.pagingPane.addedDomListeners = false;
+            self.imageDetailsPane.setElement(self.$el.find('#isic-images-imageDetailsPane')[0]);
+            self.imageDetailsPane.addedDomListeners = false;
+            self.addedTemplate = true;
+        }
+        self.imageWall.render();
+        self.pagingPane.render();
+
+        // Only show either the histogram or selected pane at a time
+        // (don't show both)
+        if (self.imageWall.selectedImageId) {
+            self.$el.find('#isic-images-imageDetailsPane').css('display', '');
+            self.imageDetailsPane.render();
+
+            self.$el.find('#isic-images-histogramPane').css('display', 'none');
+        } else {
+            self.$el.find('#isic-images-imageDetailsPane').css('display', 'none');
+
+            if (self.pagingPane.showHistograms) {
+                self.$el.find('#isic-images-histogramPane').css('display', '');
+                self.histogramPane.render();
+            } else {
+                self.$el.find('#isic-images-histogramPane').css('display', 'none');
+            }
+        }
     }
-    self.imageWall.render();
-    self.pagingPane.render();
-
-    // Only show either the histogram or selected pane at a time
-    // (don't show both)
-    if (self.imageWall.selectedImageId) {
-      self.$el.find('#isic-images-imageDetailsPane').css('display', '');
-      self.imageDetailsPane.render();
-
-      self.$el.find('#isic-images-histogramPane').css('display', 'none');
-    } else {
-      self.$el.find('#isic-images-imageDetailsPane').css('display', 'none');
-
-      if (self.pagingPane.showHistograms) {
-        self.$el.find('#isic-images-histogramPane').css('display', '');
-        self.histogramPane.render();
-      } else {
-        self.$el.find('#isic-images-histogramPane').css('display', 'none');
-      }
-    }
-  }
 });
 
 isic.router.route('images', 'images', function (id) {
--- web_external/js/views/body/ImagesView/shims/recolorImageFilters.js
+++ web_external/js/views/body/ImagesView/shims/recolorImageFilters.js
@@ -1,50 +1,54 @@
-function recolorImageFilters(colorList) {
-  var svgDefs = d3.select('body').append('svg')
-    .attr('id', 'recolorImageFilters')
-    .append('defs');
+/*globals d3*/
 
-  // Collect all colors in use
-  var allColors = {};
-  Object.keys(colorList).forEach(function(colorName) {
-    var color = colorList[colorName];
-    if (!allColors.hasOwnProperty(color)) {
-      allColors[color] = [];
-    }
-    allColors[color].push(colorName);
-  });
+function recolorImageFilters(colorList) { // eslint-disable-line no-unused-vars
+    var svgDefs = d3.select('body').append('svg')
+        .attr('id', 'recolorImageFilters')
+        .append('defs');
 
-  // Generate SVG filters that can recolor images to whatever
-  // color we need. CSS styles simply do something like
-  // filter: url(#recolorImageToFFFFFF)
-  var recolorFilters = svgDefs.selectAll('filter.recolor')
-    .data(Object.keys(allColors), function (d) { return d; });
-  var recolorFiltersEnter = recolorFilters.enter().append('filter')
-    .attr('class', 'recolor')
-    .attr('id', function (d) {
-      return 'recolorImageTo' + d.slice(1);
+    // Collect all colors in use
+    var allColors = {};
+    Object.keys(colorList).forEach(function (colorName) {
+        var color = colorList[colorName];
+        if (!allColors.hasOwnProperty(color)) {
+            allColors[color] = [];
+        }
+        allColors[color].push(colorName);
     });
-  var cmpTransferEnter = recolorFiltersEnter.append('feComponentTransfer')
-    .attr('in', 'SourceAlpha')
-    .attr('result', 'color');
-  cmpTransferEnter.append('feFuncR')
-    .attr('type', 'linear')
-    .attr('slope', 0)
-    .attr('intercept', function (d) {
-      var hexvalue = d.slice(1, 3);
-      return Math.pow(parseInt(hexvalue, 16) / 255, 2);
-    });
-  cmpTransferEnter.append('feFuncG')
-    .attr('type', 'linear')
-    .attr('slope', 0)
-    .attr('intercept', function (d) {
-      var hexvalue = d.slice(3, 5);
-      return Math.pow(parseInt(hexvalue, 16) / 255, 2);
-    });
-  cmpTransferEnter.append('feFuncB')
-    .attr('type', 'linear')
-    .attr('slope', 0)
-    .attr('intercept', function (d) {
-      var hexvalue = d.slice(5, 7);
-      return Math.pow(parseInt(hexvalue, 16) / 255, 2);
-    });
+
+    // Generate SVG filters that can recolor images to whatever
+    // color we need. CSS styles simply do something like
+    // filter: url(#recolorImageToFFFFFF)
+    var recolorFilters = svgDefs.selectAll('filter.recolor')
+        .data(Object.keys(allColors), function (d) {
+            return d;
+        });
+    var recolorFiltersEnter = recolorFilters.enter().append('filter')
+        .attr('class', 'recolor')
+        .attr('id', function (d) {
+            return 'recolorImageTo' + d.slice(1);
+        });
+    var cmpTransferEnter = recolorFiltersEnter.append('feComponentTransfer')
+        .attr('in', 'SourceAlpha')
+        .attr('result', 'color');
+    cmpTransferEnter.append('feFuncR')
+        .attr('type', 'linear')
+        .attr('slope', 0)
+        .attr('intercept', function (d) {
+            var hexvalue = d.slice(1, 3);
+            return Math.pow(parseInt(hexvalue, 16) / 255, 2);
+        });
+    cmpTransferEnter.append('feFuncG')
+        .attr('type', 'linear')
+        .attr('slope', 0)
+        .attr('intercept', function (d) {
+            var hexvalue = d.slice(3, 5);
+            return Math.pow(parseInt(hexvalue, 16) / 255, 2);
+        });
+    cmpTransferEnter.append('feFuncB')
+        .attr('type', 'linear')
+        .attr('slope', 0)
+        .attr('intercept', function (d) {
+            var hexvalue = d.slice(5, 7);
+            return Math.pow(parseInt(hexvalue, 16) / 255, 2);
+        });
 }
