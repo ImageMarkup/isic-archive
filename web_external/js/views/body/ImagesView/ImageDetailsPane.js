ece6b868b8eabf6db1eeb62e446e7e73b0c8fc40
Max Smolens <max.smolens@kitware.com>
2016-08-29 12:41:44 -0400

    image details: Add license field
    Add license field to image details. For now, hardcode to CC-0 when no license is
    specified.

--- web_external/js/views/body/ImagesView/ImageDetailsPane.js
+++ web_external/js/views/body/ImagesView/ImageDetailsPane.js
@@ -19,12 +19,25 @@ isic.views.ImagesViewSubViews.ImageDetailsPane = isic.View.extend({
 
     render: function () {
         var created = null;
+        var license = {
+            name: null,
+            url: null
+        };
         var acquisitionMetadata = null;
         var clinicalMetadata = null;
 
         // Get image data
         if (this.image.id) {
             created = girder.formatDate(this.image.get('created'), girder.DATE_SECOND);
+
+            // Get license, default to CC-0
+            if (this.image.has('license')) {
+                license.name = this.image.get('license');
+            } else {
+                license.name = 'CC-0';
+                license.url = 'https://creativecommons.org/publicdomain/zero/1.0/';
+            }
+
             var meta = this.image.get('meta');
             acquisitionMetadata = meta['acquisition'];
             clinicalMetadata = meta['clinical'];
@@ -40,6 +53,7 @@ isic.views.ImagesViewSubViews.ImageDetailsPane = isic.View.extend({
             imgRoot: girder.staticRoot + '/built/plugins/isic_archive/extra/img',
             image: this.image,
             created: created,
+            license: license,
             acquisitionMetadata: acquisitionMetadata,
             clinicalMetadata: clinicalMetadata
         }));
