990dc6d69904a1860e35b0383a0befdbe022ddbd
Max Smolens <max.smolens@kitware.com>
2016-09-02 13:26:01 -0400

    ImagesView: Prefer binding 'this' over 'self' variable

--- web_external/js/views/body/ImagesView/index.js
+++ web_external/js/views/body/ImagesView/index.js
@@ -2,56 +2,53 @@ isic.views.ImagesViewSubViews = isic.views.ImagesViewSubViews || {};
 
 isic.views.ImagesView = isic.View.extend({
     initialize: function () {
-        var self = this;
-        self.model = new isic.views.ImagesViewSubViews.ImagesViewModel();
+        this.model = new isic.views.ImagesViewSubViews.ImagesViewModel();
 
         // Initialize our subviews
         var params = {
-            model: self.model,
+            model: this.model,
             parentView: this
         };
-        self.studyPane = new isic.views.ImagesViewSubViews.StudyPane(params);
-        self.histogramPane = new isic.views.ImagesViewSubViews.HistogramPane(params);
-        self.imageWall = new isic.views.ImagesViewSubViews.ImageWall(params);
-        self.pagingPane = new isic.views.ImagesViewSubViews.PagingPane(params);
-        self.imageDetailsPane = new isic.views.ImagesViewSubViews.ImageDetailsPane(params);
+        this.studyPane = new isic.views.ImagesViewSubViews.StudyPane(params);
+        this.histogramPane = new isic.views.ImagesViewSubViews.HistogramPane(params);
+        this.imageWall = new isic.views.ImagesViewSubViews.ImageWall(params);
+        this.pagingPane = new isic.views.ImagesViewSubViews.PagingPane(params);
+        this.imageDetailsPane = new isic.views.ImagesViewSubViews.ImageDetailsPane(params);
 
-        window.onresize = function () {
-            self.render();
-        };
-        self.listenTo(self.model, 'change:selectedImageId', self.toggleDetailsPane);
+        window.onresize = _.bind(function () {
+            this.render();
+        }, this);
+        this.listenTo(this.model, 'change:selectedImageId', this.toggleDetailsPane);
 
-        self.render();
+        this.render();
     },
     toggleDetailsPane: function () {
-        var self = this;
-        if (self.model.get('selectedImageId') !== null) {
-            self.$el.find('#isic-images-imageDetailsPane').css('display', '');
+        if (this.model.get('selectedImageId') !== null) {
+            this.$el.find('#isic-images-imageDetailsPane').css('display', '');
         } else {
-            self.$el.find('#isic-images-imageDetailsPane').css('display', 'none');
+            this.$el.find('#isic-images-imageDetailsPane').css('display', 'none');
         }
     },
     render: function () {
-        var self = this;
-        if (!(self.addedTemplate)) {
-            self.$el.html(isic.templates.imagesPage({
+        if (!(this.addedTemplate)) {
+            this.$el.html(isic.templates.imagesPage({
                 staticRoot: girder.staticRoot
             }));
             window.shims.recolorImageFilters(['#00ABFF', '#444499', '#CCCCCC']);
-            self.studyPane.setElement(self.$el.find('#isic-images-studyPane')[0]);
-            self.histogramPane.setElement(self.$el.find('#isic-images-histogramPane')[0]);
-            self.imageWall.setElement(self.$el.find('#isic-images-imageWall')[0]);
-            self.pagingPane.setElement(self.$el.find('#isic-images-pagingPane')[0]);
-            self.imageDetailsPane.setElement(self.$el.find('#isic-images-imageDetailsPane')[0]);
-            self.addedTemplate = true;
+            this.studyPane.setElement(this.$el.find('#isic-images-studyPane')[0]);
+            this.histogramPane.setElement(this.$el.find('#isic-images-histogramPane')[0]);
+            this.imageWall.setElement(this.$el.find('#isic-images-imageWall')[0]);
+            this.pagingPane.setElement(this.$el.find('#isic-images-pagingPane')[0]);
+            this.imageDetailsPane.setElement(this.$el.find('#isic-images-imageDetailsPane')[0]);
+            this.addedTemplate = true;
         }
-        self.imageWall.render();
-        self.pagingPane.render();
-        self.studyPane.render();
-        self.histogramPane.render();
-        self.imageDetailsPane.render();
+        this.imageWall.render();
+        this.pagingPane.render();
+        this.studyPane.render();
+        this.histogramPane.render();
+        this.imageDetailsPane.render();
 
-        self.toggleDetailsPane();
+        this.toggleDetailsPane();
 
         return this;
     }
