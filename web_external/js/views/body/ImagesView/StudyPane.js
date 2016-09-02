4990aae52d97544ddccfb80bad546385574e59f7
Max Smolens <max.smolens@kitware.com>
2016-09-02 12:37:32 -0400

    StudyPane: Prefer binding 'this' over 'self' variable

--- web_external/js/views/body/ImagesView/StudyPane.js
+++ web_external/js/views/body/ImagesView/StudyPane.js
@@ -4,43 +4,40 @@ isic.views.ImagesViewSubViews = isic.views.ImagesViewSubViews || {};
 
 isic.views.ImagesViewSubViews.StudyPane = Backbone.View.extend({
     initialize: function () {
-        var self = this;
-
-        self.listenTo(self.model, 'change:overviewHistogram', self.render);
-        self.listenTo(self.model, 'change:filteredSetHistogram', self.render);
-        self.listenTo(self.model, 'change:pageHistogram', self.render);
+        this.listenTo(this.model, 'change:overviewHistogram', this.render);
+        this.listenTo(this.model, 'change:filteredSetHistogram', this.render);
+        this.listenTo(this.model, 'change:pageHistogram', this.render);
     },
     render: function () {
-        var self = this;
         var svg;
 
-        if (!self.addedSvgElement) {
+        if (!this.addedSvgElement) {
             this.$el.html(isic.templates.imageStudyPage({
                 title: 'Dataset'
             }));
             var histogramContainer = this.$('.isic-image-study-histogram-container');
             svg = d3.select(histogramContainer.get(0)).append('svg')
                 .attr('class', 'content');
-            self.histogram = new isic.views.ImagesViewSubViews.IndividualHistogram({
+            this.histogram = new isic.views.ImagesViewSubViews.IndividualHistogram({
                 el: svg.node(),
-                model: self.model,
+                model: this.model,
                 attributeName: 'folderId'
             });
-            self.addedSvgElement = true;
+            this.addedSvgElement = true;
         } else {
-            svg = d3.select(self.el).select('svg.content');
+            svg = d3.select(this.el).select('svg.content');
         }
-        self.histogram.render();
+        this.histogram.render();
 
         // Add special listeners to open a modal about each study
         svg.select('.bins').selectAll('.bin')
-            .on('click', function (d) {
-                var dataset = self.model.datasetCollection.find(function (dataset) {
+            .on('click', _.bind(function (d) {
+                var dataset = this.model.datasetCollection.find(function (dataset) {
                     return dataset.name() === d;
                 });
                 console.log('TODO: show a modal, describing the ' +
                     d + ' study (folder id: ' + dataset.id + ')');
-            });
+            }, this));
         return this;
     }
 });
