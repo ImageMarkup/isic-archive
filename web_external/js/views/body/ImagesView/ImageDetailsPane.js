a4f952cd6d7e8a345159ddb4b924b24c61fb1ee3
Roni Choudhury <roni.choudhury@kitware.com>
2016-09-13 17:52:00 -0400

    Use a lightbox (really a Bootstrap modal) to show an image

--- web_external/js/views/body/ImagesView/ImageDetailsPane.js
+++ web_external/js/views/body/ImagesView/ImageDetailsPane.js
@@ -70,6 +70,42 @@ isic.views.ImagesViewSubViews.ImageDetailsPane = isic.View.extend({
       window.open('/api/v1/image/' + this.image.id + '/download?contentDisposition=inline');
     },
 
+    fullscreen: function () {
+      var img = $('.focusimage');
+      var modal = $('#focusmodal');
+
+      // Supply the image element with a new src attribute to display the image.
+      var src = '/api/v1/image/' + this.image.id + '/download?contentDisposition=inline';
+      img.attr('src', src);
+
+      // Create a dummy image element so we can learn the size of the new image.
+      // When the image is loaded, we can use its size to properly resize and
+      // situate the modal.
+      var image = new Image();
+      image.onload = function () {
+        // Add thirty to represent the 15px padding that comes stock with a
+        // Bootstrap modal.
+        var modalWidth = image.width + 30;
+        var modalHeight = image.height + 30;
+
+        // Set the CSS of the dialog so it appears vertically and horizontally
+        // centered in the webpage (even with browser resizing).
+        modal.find('.modal-dialog').css({
+          width: modalWidth + 'px',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          'margin-top': (-modalHeight / 2) + 'px',
+          'margin-left': (-modalWidth / 2) + 'px'
+        });
+
+        // Now that the image and CSS are ready, show the modal.
+        modal.modal('show');
+      };
+
+      image.src = src;
+    },
+
     clearSelectedImage: function () {
         this.image.clear();
     }
