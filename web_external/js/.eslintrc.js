21109836f4f699fe2c586189079636da7c9170b9
Brian Helba <brian.helba@kitware.com>
2016-09-20 08:02:37 -0400

    Find Girder base ".eslintrc" in a more robust way
    Fixes #106.

--- web_external/js/.eslintrc.js
+++ web_external/js/.eslintrc.js
@@ -1,8 +1,29 @@
+var fs = require('fs');
 var path = require('path');
 
+var pluginPath = path.basename(path.basename(__dirname));
+var possibleGirderPaths = [
+    path.basename(path.basename(pluginPath)),
+    path.join(path.basename(pluginPath), 'girder'),
+    process.cwd()
+];
+
+var configPath = possibleGirderPaths
+    .map(function (possibleGirderPath) {
+        return path.join(possibleGirderPath, '.eslintrc');
+    })
+    .find(function (configPath) {
+        try {
+            fs.accessSync(configPath);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    });
+
 module.exports = {
-    "extends": path.join(process.cwd(), ".eslintrc"),
-    "globals": {
-        "isic": true
+    'extends': configPath,
+    'globals': {
+        'isic': true
     }
 };
