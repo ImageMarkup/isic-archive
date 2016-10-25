df4a2c1008e35337412d2f8da03af57ddadfee5d
Brian Helba <brian.helba@kitware.com>
2016-10-25 06:00:12 -0400

    Improve D3 data binding in ImageWall

--- web_external/js/views/body/ImagesView/ImageWall.js
+++ web_external/js/views/body/ImagesView/ImageWall.js
@@ -9,10 +9,6 @@ isic.views.ImagesViewSubViews = isic.views.ImagesViewSubViews || {};
 isic.views.ImagesViewSubViews.ImageWall = isic.View.extend({
     initialize: function (settings) {
         this.image = settings.image;
-        this.imageCache = {};
-        this.loadedImages = {};
-        this.imageColumnLookup = {};
-        this.imageColumns = [];
 
         this.listenTo(this.image, 'change:_id', this.render);
         this.listenTo(this.model.images, 'g:changed', this.render);
@@ -25,22 +21,25 @@ isic.views.ImagesViewSubViews.ImageWall = isic.View.extend({
         }
     },
     render: _.debounce(function () {
-        var self = this;
-
         d3.select(this.el)
           .selectAll('img')
           .remove();
 
         var sel = d3.select(this.el)
             .selectAll('img')
-            .data(this.model.images.pluck('_id'));
+            .data(this.model.images.map(function (image) {
+                return {
+                    id: image.id,
+                    name: image.get('name')
+                };
+            }));
 
         sel.enter()
             .append('img')
             .classed('thumb', true);
 
         sel.attr('src', function (d) {
-            return girder.apiRoot + '/image/' + d + '/thumbnail?width=' + imageSize;
+            return girder.apiRoot + '/image/' + d.id + '/thumbnail?width=' + imageSize;
         })
             .attr('height', imageSize * 0.75)
             .attr('width', imageSize)
@@ -48,29 +47,23 @@ isic.views.ImagesViewSubViews.ImageWall = isic.View.extend({
             .attr('data-placement', 'auto')
             .attr('data-viewport', '#isic-images-imageWall')
             .classed('selected', _.bind(function (d) {
-                return d === this.image.id;
+                return d.id === this.image.id;
             }, this))
             .on('click', _.bind(function (d) {
                 this.clearTooltips();
                 if (d3.event.shiftKey) {
-                    var image = new isic.models.ImageModel({
-                        _id: d
-                    });
                     new isic.views.ImageFullscreenWidget({ // eslint-disable-line no-new
                         el: $('#g-dialog-container'),
-                        model: image,
+                        model: this.model.images.findWhere({_id: d.id}),
                         parentView: this
                     });
                 } else {
-                    this.selectImage(d === this.image.id ? null : d);
+                    this.selectImage(d.id === this.image.id ? null : d.id);
                 }
             }, this))
             .each(function (d) {
-                var imageCollection = self.model.images;
-                var imageModel = imageCollection.find(function (x) { return x.id === d; });
-
                 $(this).tooltip({
-                    title: imageModel.get('name')
+                    title: d.name
                 });
             });
     }, 50),
