92f7af8fba73ab01b27fe92870dba6955b012830
Brian Helba <brian.helba@kitware.com>
2016-12-02 15:13:55 -0500

    Specify a query string parameter in a more appropriate place

--- web_external/js/views/body/TasksView.js
+++ web_external/js/views/body/TasksView.js
@@ -67,7 +67,7 @@ isic.views.TasksView = isic.View.extend({
 
         if (girder.currentUser.getSegmentationSkill() !== null) {
             this.segmentationTasks = new isic.collections.TaskCollection();
-            this.segmentationTasks.altUrl = 'task/me/segmentation?details=false';
+            this.segmentationTasks.altUrl = 'task/me/segmentation';
             this.segmentationTasks.pageLimit = Number.MAX_SAFE_INTEGER;
 
             this.taskSegmentationView = new isic.views.TasksGroupView({
@@ -126,7 +126,9 @@ isic.views.TasksView = isic.View.extend({
             this.reviewTasks.fetch();
         }
         if (this.segmentationTasks) {
-            this.segmentationTasks.fetch();
+            this.segmentationTasks.fetch({
+                details: false
+            });
         }
         this.annotationTasks.fetch();
     }
