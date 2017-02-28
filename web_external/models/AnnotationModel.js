f2a343e01b3bb3458258ef41ccbcfaf0cdab030e
Brian Helba <brian.helba@kitware.com>
2017-02-28 00:00:43 -0500

    WIP: Refactor study results local features

--- web_external/models/AnnotationModel.js
+++ web_external/models/AnnotationModel.js
@@ -1,3 +1,11 @@
 isic.models.AnnotationModel = girder.Model.extend({
-    resourceName: 'annotation'
+    resourceName: 'annotation',
+
+    isComplete: function () {
+        return this.get('state') === 'complete';
+    },
+
+    image: function () {
+        return new isic.models.ImageModel(this.get('image'));
+    }
 });
