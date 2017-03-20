9ff4d5ee986e9a821d910c13e78e39623b2fe7b1
Brian Helba <brian.helba@kitware.com>
2017-03-19 22:05:22 -0400

    Use Backbone destroy, instead of Girder's method

--- web_external/model.js
+++ web_external/model.js
@@ -1,5 +1,8 @@
 isic.Model = girder.Model.extend({
     urlRoot: function () {
         return this.resourceName;
-    }
+    },
+
+    // "girder.Model.destroy" doesn't trigger many of the proper Backbone events
+    destroy: Backbone.Model.prototype.destroy
 });
