c9702d496ea787be3356a4295184eeca56055dd8
Brian Helba <brian.helba@kitware.com>
2017-03-27 17:07:36 -0400

    Move ImagesView event handlers to their own functions

--- web_external/ImagesGallery/ImagesView.js
+++ web_external/ImagesGallery/ImagesView.js
@@ -11,50 +11,8 @@ isic.views.ImagesView = isic.View.extend({
         this.images = new isic.collections.SelectableImageCollection();
         this.images.pageLimit = 50;
 
-        // Rather than issue a second fetch request for this.filteredFacets,
-        // copy the response of the first request for this.completeFacets
-        this.listenTo(this.completeFacets, 'sync', function (collection, resp, options) {
-            // TODO: ideally, this.filteredFacets would only be reset when
-            // this.completeFacets triggered an "update" (which would prevent resets when
-            // "sync" had no changes); however, it's difficult to deep-copy all of
-            // the models from this.completeFacets to this.filteredFacets without having
-            // access to the raw HTTP response
-            this.filteredFacets.reset(resp, {parse: true});
-
-            this.images.fetch();
-
-            this.filters.initialize(collection);
-        });
-        this.listenTo(this.filters, 'change', function () {
-            // Cancel any previous still-pending requests, as this new set of requests will override
-            // them anyway
-            _.each([this.filteredFacets, this.images], function (collection) {
-                if (collection._pendingRequest) {
-                    collection._pendingRequest.abort();
-                }
-            });
-
-            // TODO: If there's no "filterQuery", we could just always reset "filteredFacets" to
-            // "completeFacets" here; then make "filters.initialize" trigger a "change" event
-            // when called; then remove the calls to "images.fetch" and "filteredFacets.reset"
-            // from the "completeFacets:sync" event handler and let them just run from here only
-
-            var filterQuery = JSON.stringify(this.filters.asAst());
-            this.filteredFacets._pendingRequest = this.filteredFacets.fetch({
-                // filteredFacets is a direct subclass of Backbone.Collection, with different
-                // arguments to ".fetch"
-                data: {
-                    filter: filterQuery
-                }
-            });
-            this.images._pendingRequest = this.images.fetch({
-                offset: 0,
-                filter: filterQuery
-            });
-
-            // TODO: It would be nice if collections just automatically stored their most recent
-            // fetch XHR as an internal property.
-        });
+        this.listenTo(this.completeFacets, 'sync', this.onCompleteFacetsFetched);
+        this.listenTo(this.filters, 'change', this.onFiltersChanged);
         this.listenTo(this.images, 'select:one', this.onImageSelected);
         // Collection resets do not trigger "deselect" events, so they must be listened for
         this.listenTo(this.images, 'deselect:one reset', this.onImageDeselected);
@@ -122,6 +80,52 @@ isic.views.ImagesView = isic.View.extend({
         // completely loaded before "images" resolves
 
         return this;
+    },
+
+    onCompleteFacetsFetched: function (collection, resp, options) {
+        // Rather than issue a second fetch request for this.filteredFacets,
+        // copy the response of the first request for this.completeFacets
+        // TODO: ideally, this.filteredFacets would only be reset when
+        // this.completeFacets triggered an "update" (which would prevent resets when
+        // "sync" had no changes); however, it's difficult to deep-copy all of
+        // the models from this.completeFacets to this.filteredFacets without having
+        // access to the raw HTTP response
+        this.filteredFacets.reset(resp, {parse: true});
+
+        this.images.fetch();
+
+        this.filters.initialize(collection);
+    },
+
+    onFiltersChanged: function () {
+        // Cancel any previous still-pending requests, as this new set of requests will override
+        // them anyway
+        _.each([this.filteredFacets, this.images], function (collection) {
+            if (collection._pendingRequest) {
+                collection._pendingRequest.abort();
+            }
+        });
+
+        // TODO: If there's no "filterQuery", we could just always reset "filteredFacets" to
+        // "completeFacets" here; then make "filters.initialize" trigger a "change" event
+        // when called; then remove the calls to "images.fetch" and "filteredFacets.reset"
+        // from the "onCompleteFacetsFetched" handler and let them just run from here only
+
+        var filterQuery = JSON.stringify(this.filters.asAst());
+        this.filteredFacets._pendingRequest = this.filteredFacets.fetch({
+            // filteredFacets is a direct subclass of Backbone.Collection, with different
+            // arguments to ".fetch"
+            data: {
+                filter: filterQuery
+            }
+        });
+        this.images._pendingRequest = this.images.fetch({
+            offset: 0,
+            filter: filterQuery
+        });
+
+        // TODO: It would be nice if collections just automatically stored their most recent
+        // fetch XHR as an internal property.
     },
 
     _clearDetailsPane: function () {
