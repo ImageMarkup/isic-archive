7d7a9d536fc16e244c1fb736afd547ce3a6306b3
Brian Helba <brian.helba@kitware.com>
2017-03-28 14:08:43 -0400

    Fix style error

--- web_external/ImagesGallery/ImagesFacetCollection.js
+++ web_external/ImagesGallery/ImagesFacetCollection.js
@@ -17,7 +17,7 @@ isic.collections.ImagesFacetCollection = Backbone.Collection.extend({
             .map(function (facetBins, facetId) {
                 return {
                     id: facetId,
-                    bins: _.map(facetBins, function(facetBin) {
+                    bins: _.map(facetBins, function (facetBin) {
                         // Map a label of a null object to a "__null__" string because:
                         // * The null object cannot be stored on DOM elements as a data property
                         // * The null object will be coerced to a string of "null" in any places
