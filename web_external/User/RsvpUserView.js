ecebeabab14bda0b899552f1dab4e3558bbaf11f
Max Smolens <max.smolens@kitware.com>
2017-04-12 11:35:51 -0400

    Polish RSVP view

--- web_external/User/RsvpUserView.js
+++ web_external/User/RsvpUserView.js
@@ -2,38 +2,40 @@ isic.views.RsvpUserView = isic.View.extend({
     events: {
         'submit #isic-user-rsvp-form': function (event) {
             event.preventDefault();
+            this.$('#isic-user-rsvp-submit').prop('disabled', true);
 
             if (this.$('#isic-user-rsvp-password').val() !== this.$('#isic-user-rsvp-password2').val()) {
                 isic.showAlertDialog({
                     text: 'Passwords do not match, try again.',
                     callback: _.bind(function () {
-                        this.$('#isic-user-rsvp-password,#isic-user-rsvp-password2').val('');
-                        this.$('#isic-user-rsvp-password').focus();
+                        this.$('#isic-user-rsvp-password2').val('').focus();
                     }, this)
                 });
+                this.$('#isic-user-rsvp-submit').prop('disabled', false);
                 return;
             }
 
             this.user
                 .changePassword(
                     this.token,
-                    this.$('#g-password-new').val()
+                    this.$('#isic-user-rsvp-password').val()
                 )
                 .done(function () {
                     isic.router.navigate('tasks', {trigger: true});
                 })
-                .fail(function (resp) {
+                .fail(_.bind(function (resp) {
                     isic.showAlertDialog({
                         text: '<h4>Error changing password</h4><br>' + _.escape(resp.responseJSON.message),
                         escapedHtml: true
                     });
-                });
+                    this.$('#isic-user-rsvp-submit').prop('disabled', false);
+                }, this));
         }
     },
 
     /**
      * @param {isic.models.UserModel} settings.user - The user to complete registration for.
-     * @param {string} settings.token - The user to complete registration for.
+     * @param {string} settings.token - The TEMPORARY_USER_AUTH token.
      */
     initialize: function (settings) {
         this.user = settings.user;
@@ -47,6 +49,8 @@ isic.views.RsvpUserView = isic.View.extend({
             user: this.user
         }));
 
+        this.$('#isic-user-rsvp-password').focus();
+
         return this;
     }
 });
@@ -55,16 +59,16 @@ isic.router.route('user/:id/rsvp/:token', 'rsvpUser', function (id, token) {
     isic.models.UserModel
         .temporaryTokenLogin(id, token)
         .done(function (resp) {
-            girder.events.trigger('g:navigateTo', girder.views.RsvpUserView, {
+            girder.events.trigger('g:navigateTo', isic.views.RsvpUserView, {
                 user: girder.currentUser,
                 token: token
             });
         })
-        .fail(function () {
+        .fail(function (resp) {
             isic.showAlertDialog({
                 text: '<h4>Error loading user from token</h4><br>' + _.escape(resp.responseJSON.message),
                 escapedHtml: true
             });
-            girder.router.navigate('', {trigger: true});
+            isic.router.navigate('', {trigger: true});
         });
 });
