d6930a036f583787969d8269a0db4b7a2672df65
Brian Helba <brian.helba@kitware.com>
2017-08-22 03:11:11 -0400

    Define plugin tests with add_standard_plugin_tests and add stylint tests

--- plugin.cmake
+++ plugin.cmake
@@ -14,61 +14,42 @@
 #  limitations under the License.
 ###############################################################################
 
-add_python_style_test(
-  python_static_analysis_isic_archive
-  "${CMAKE_CURRENT_LIST_DIR}/server")
-add_python_style_test(
-  python_static_analysis_isic_archive_tests
-  "${CMAKE_CURRENT_LIST_DIR}/plugin_tests")
+add_standard_plugin_tests()
+
+# Fetch external data
+set(_data_files "")
+foreach(_data_file
+  "plugins/isic_archive/test_1_small_1.jpg"
+  "plugins/isic_archive/test_1_small_2.jpg"
+  "plugins/isic_archive/test_1_small_3.jpg"
+  "plugins/isic_archive/test_1_large_1.jpg"
+  "plugins/isic_archive/test_1_large_2.jpg"
+  "plugins/isic_archive/test_1_metadata.csv"
+)
+  list(APPEND _data_files "DATA{${GIRDER_EXTERNAL_DATA_BUILD_PATH}/${_data_file}}")
+endforeach()
+girder_ExternalData_expand_arguments("${name}_data" _tmp ${_data_files})
+girder_ExternalData_add_target("${name}_data")
+
+# Other Python code
 add_python_style_test(
   python_static_analysis_isic_archive_scripts
   "${CMAKE_CURRENT_LIST_DIR}/scripts")
 
-add_python_test(
-  user
-  PLUGIN isic_archive)
-add_python_test(
-  upload
-  PLUGIN isic_archive
-  EXTERNAL_DATA
-    "plugins/isic_archive/test_1_small_1.jpg"
-    "plugins/isic_archive/test_1_small_2.jpg"
-    "plugins/isic_archive/test_1_small_3.jpg"
-    "plugins/isic_archive/test_1_large_1.jpg"
-    "plugins/isic_archive/test_1_large_2.jpg"
-    "plugins/isic_archive/test_1_metadata.csv")
-add_python_test(
-  featureset
-  PLUGIN isic_archive)
-add_python_test(
-  segmentation_helper
-  PLUGIN isic_archive)
-add_python_test(
-  utility
-  PLUGIN isic_archive)
-
+# External client static analysis
 add_eslint_test(
   isic_archive_external
   "${CMAKE_CURRENT_LIST_DIR}/web_external"
-  ESLINT_CONFIG_FILE "${CMAKE_CURRENT_LIST_DIR}/.eslintrc"
-  ESLINT_IGNORE_FILE "${CMAKE_CURRENT_LIST_DIR}/.eslintignore")
-#add_puglint_test(
-#  isic_archive_external
-#  "${CMAKE_CURRENT_LIST_DIR}/web_external")
+  ESLINT_CONFIG_FILE "${CMAKE_CURRENT_LIST_DIR}/.eslintrc")
 add_test(
   NAME puglint_isic_archive_external
   WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
   COMMAND "${PUGLINT_EXECUTABLE}" -c "${CMAKE_CURRENT_LIST_DIR}/.pug-lintrc" "${CMAKE_CURRENT_LIST_DIR}/web_external")
-add_eslint_test(
-  isic_archive
-  "${CMAKE_CURRENT_LIST_DIR}/web_client/js")
-#add_puglint_test(
-#  isic_archive
-#  "${CMAKE_CURRENT_LIST_DIR}/web_client/templates")
-add_test(
-  NAME puglint_isic_archive
-  WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-  COMMAND "${PUGLINT_EXECUTABLE}" -c "${CMAKE_CURRENT_LIST_DIR}/.pug-lintrc" "${CMAKE_CURRENT_LIST_DIR}/web_client/templates")
+add_stylint_test(
+  isic_archive_external
+  "${CMAKE_CURRENT_LIST_DIR}/web_external")
+
+# Other Javascript code
 add_eslint_test(
   isic_archive_grunt
   "${CMAKE_CURRENT_LIST_DIR}/Gruntfile.js"
