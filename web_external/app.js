551a816e09ad9d3cc06fa9423f1311b5fef8a514
Brian Helba <brian.helba@kitware.com>
2017-10-20 19:11:37 -0400

    Use upstream Backbone.sync

--- web_external/app.js
+++ web_external/app.js
@@ -117,49 +117,6 @@ const IsicApp = GirderApp.extend({
     }
 });
 
-Backbone.sync = function (method, model, options) {
-    // In order to use the native "Backbone.Model.destroy" method (which triggers the correct
-    // collection-level events, unlike the Girder version), a working "Backbone.sync" method is
-    // required. Since all Ajax calls must be made via "restRequest" (to add auth headers)
-    // and since "Backbone.ajax" cannot be directly changed to use "restRequest" (since
-    // "restRequest actually calls "Backbone.ajax"), "Backbone.sync" must be reimplemented to
-    // use "restRequest" directly.
-    // In this reimplementation, the only important changes are:
-    //   * Use "restRequest" instead of "Backbone.ajax"
-    //   * Set "params.path" instead of "params.url"
-    let methodMap = {
-        'create': 'POST',
-        'update': 'PUT',
-        'patch': 'PATCH',
-        'delete': 'DELETE',
-        'read': 'GET'
-    };
-    let type = methodMap[method];
-
-    options = options || {};
-
-    let params = {type: type, dataType: 'json'};
-
-    if (!options.url) {
-        // params.url = _.result(model, 'url') || urlError();
-        // restRequest expects a "path" option, and will set "url" internally
-        params.path = _.result(model, 'url');
-    }
-
-    if (options.data === null && model && (method === 'create' || method === 'update' || method === 'patch')) {
-        params.contentType = 'application/json';
-        params.data = JSON.stringify(options.attrs || model.toJSON(options));
-    }
-
-    if (params.type !== 'GET') {
-        params.processData = false;
-    }
-
-    let xhr = options.xhr = restRequest(_.extend(params, options));
-    model.trigger('request', model, xhr, options);
-    return xhr;
-};
-
 /**
  * Patch ItemModel with a method to get the files within the item.
  */
