ea1ddff108716afc3e376806baab30a4c79c7b8e
Brian Helba <brian.helba@kitware.com>
2017-11-06 00:31:24 -0500

    Create sections for gallery facet types

--- web_external/ImagesGallery/Facets/ImagesFacetsPane.js
+++ web_external/ImagesGallery/Facets/ImagesFacetsPane.js
@@ -19,14 +19,16 @@ const ImagesFacetsPane = View.extend({
         // TODO: Use the more general 'update' event, once Girder's version of Backbone is upgraded
         // TODO: ensure this fires when facets are modified in-place
         this.listenTo(this.completeFacets, 'sync', this.render);
+
+        this._facetViews = [];
     },
 
     render: function () {
         _.each(this.facetViews, (facetView) => {
             facetView.destroy();
         });
+        this._facetViews = [];
         this.$el.empty();
-        delete this.facetViews;
 
         this.$el.html(ImagesFacetsPaneTemplate({
             filterHexColors: [
@@ -35,27 +37,47 @@ const ImagesFacetsPane = View.extend({
             ]
         }));
 
-        this.facetViews = this.completeFacets.map((completeFacet) => {
-            let facetId = completeFacet.id;
-            let filteredFacet = this.filteredFacets.get(facetId);
-            let facetFilter = this.filters.facetFilter(facetId);
+        this._createFacetView('meta.clinical.diagnosis')
+            .$el.insertBefore(this.$('.isic-images-facets-clinical'));
+        this.completeFacets.forEach((completeFacet) => {
+            const facetId = completeFacet.id;
 
-            let FacetView = completeFacet.schema().FacetView;
-            let facetView = new FacetView({
-                completeFacet: completeFacet,
-                filteredFacet: filteredFacet,
-                filter: facetFilter,
-                parentView: this
-            });
+            let headerEl = null;
+            // TODO: Use String.startswith and a ES6 polyfill
+            if (facetId.indexOf('meta.clinical') !== -1) {
+                if (facetId === 'meta.clinical.diagnosis') {
+                    // Special case
+                    return;
+                }
+                headerEl = this.$('.isic-images-facets-clinical');
+            } else if (facetId.indexOf('meta.acquisition') !== -1) {
+                headerEl = this.$('.isic-images-facets-acquisition');
+            } else {
+                headerEl = this.$('.isic-images-facets-database');
+            }
 
-            this.$el.append(facetView.el);
-            // Do not render until the view has been inserted into the main DOM
+            this._createFacetView(facetId)
+                .$el.appendTo(headerEl);
+        });
+
+        _.each(this._facetViews, (facetView) => {
             facetView.render();
-
-            return facetView;
         });
 
         return this;
+    },
+
+    _createFacetView: function (facetId) {
+        const completeFacet = this.completeFacets.get(facetId);
+        const FacetView = completeFacet.schema().FacetView;
+        const facetView = new FacetView({
+            completeFacet: completeFacet,
+            filteredFacet: this.filteredFacets.get(facetId),
+            filter: this.filters.facetFilter(facetId),
+            parentView: this
+        });
+        this._facetViews.push(facetView);
+        return facetView;
     }
 });
 
