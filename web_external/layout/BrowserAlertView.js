438581112cf5631afd001deb5ab533cc5e680c9e
Brian Helba <brian.helba@kitware.com>
2017-11-06 19:57:20 -0500

    Add browser detection and warning message

---
+++ web_external/layout/BrowserAlertView.js
@@ -0,0 +1,28 @@
+import _ from 'underscore';
+import UAParser from 'ua-parser-js';
+
+import View from '../view';
+
+import BrowserAlertTemplate from './browserAlert.pug';
+import './browserAlert.styl';
+
+const BrowserAlertView = View.extend({
+    className: 'isic-browser-alert',
+
+    initialize: function (settings) {
+        this.currentBrowserName = (new UAParser()).getBrowser().name;
+        this.unsupportedBrowsers = [
+            'IE',
+            'Safari'
+        ];
+    },
+
+    render: function () {
+        if (_.contains(this.unsupportedBrowsers, this.currentBrowserName)) {
+            this.$el.html(BrowserAlertTemplate());
+        }
+        return this;
+    }
+});
+
+export default BrowserAlertView;
