9890aaaf0037c0bb99ddac195428adfac70f27b1
Brian Helba <brian.helba@kitware.com>
2018-02-27 23:52:40 -0500

    Move Annotation to an isolated model type

---
+++ scripts/migrateAnnotation.py
@@ -0,0 +1,27 @@
+from girder.utility import server as girder_server
+girder_server.configureServer()
+
+from girder.models.item import Item  # noqa: E402
+from girder.models.folder import Folder  # noqa: E402
+from girder.plugins.isic_archive.models.annotation import Annotation  # noqa: E402
+from girder.plugins.isic_archive.models.study import Study  # noqa: E402
+
+for annotationItem in Item().find({
+    'baseParentId': Study().loadStudyCollection()['_id']
+}):
+    dataset = Annotation().save({
+        '_id': annotationItem['_id'],
+        'studyId': annotationItem['meta']['studyId'],
+        'imageId': annotationItem['meta']['studyId'],
+        'userId': annotationItem['meta']['studyId'],
+        'startTime': annotationItem['meta']['startTime'],
+        'stopTime': annotationItem['meta']['startTime'],
+        'status': annotationItem['meta']['startTime'],
+        'annotations': annotationItem['meta']['startTime'],
+    })
+
+for studyAnnotatorFolder in Folder().find({
+    'baseParentId': Study().loadStudyCollection()['_id'],
+    'meta.userId': {'$exists': 1}
+}):
+    Folder().remove(studyAnnotatorFolder)
--- server/models/annotation.py
+++ server/models/annotation.py
@@ -23,47 +23,47 @@ import six
 
 from bson import ObjectId
 
-from girder.models.item import Item
 from girder.exceptions import GirderException, ValidationException
+from girder.models.model_base import Model
+from girder.utility.acl_mixin import AccessControlMixin
 
 from .image import Image
 from .study import Study
 from .user import User
 
 
-class Annotation(Item):
-    def createAnnotation(self, study, image, creatorUser,
-                         annotatorFolder):
-        annotationItem = self.createItem(
-            folder=annotatorFolder,
-            name=image['name'],
-            description='',
-            creator=creatorUser
-        )
-        # "setMetadata" will always save
-        self.setMetadata(
-            item=annotationItem,
-            metadata={
-                'studyId': study['_id'],
-                'userId': annotatorFolder['meta']['userId'],
-                'imageId': image['_id'],
-                'startTime': None,
-                'stopTime': None,
-                'annotations': None
-            }
-        )
-        return annotationItem
+class Annotation(AccessControlMixin, Model):
+    def initialize(self):
+        self.name = 'annotation'
+        self.ensureIndices(['studyId', 'imageId', 'userId'])
+
+        # TODO: resourceColl should be ['study', 'isic_archive'], but upstream support is unclear
+        self.resourceColl = 'folder'
+        self.resourceParent = 'studyId'
+
+    def createAnnotation(self, study, image, user):
+        annotation = self.save({
+            'studyId': study['_id'],
+            'imageId': image['_id'],
+            'userId': user['_id'],
+            'startTime': None,
+            'stopTime': None,
+            'status': None,
+            'annotations': None
+        })
+
+        return annotation
 
     def getState(self, annotation):
         return (Study().State.COMPLETE
-                if annotation['meta'].get('stopTime') is not None
+                if annotation['stopTime'] is not None
                 else Study().State.ACTIVE)
 
     def _getImageMasks(self, annotation, featureId, image=None):
         if self.getState(annotation) != Study().State.COMPLETE:
             raise GirderException('Annotation is incomplete.')
 
-        featureValues = annotation['meta']['annotations'].get(featureId, [])
+        featureValues = annotation['annotations'].get(featureId, [])
         if not isinstance(featureValues, list):
             raise GirderException(
                 'Feature %s is not a superpixel annotation.' % featureId)
@@ -82,7 +82,7 @@ class Annotation(Item):
         ])
 
         if not image:
-            image = Image().load(annotation['meta']['imageId'], force=True, exc=True)
+            image = Image().load(annotation['imageId'], force=True, exc=True)
         superpixelsData = Image().superpixelsData(image)
 
         possibleMask = numpy.in1d(
@@ -108,7 +108,7 @@ class Annotation(Item):
         return renderedMask
 
     def renderAnnotation(self, annotation, featureId):
-        image = Image().load(annotation['meta']['imageId'], force=True, exc=True)
+        image = Image().load(annotation['imageId'], force=True, exc=True)
         renderData = Image().imageData(image)
 
         possibleMask, definiteMask = self._getImageMasks(
@@ -122,31 +122,16 @@ class Annotation(Item):
 
         return renderData
 
-    def _findQueryFilter(self, query):
-        newQuery = query.copy() if query is not None else {}
-        newQuery.update({
-            'baseParentId': Study().loadStudyCollection()['_id']
-        })
-        return newQuery
-
-    def find(self, query=None, **kwargs):
-        annotationQuery = self._findQueryFilter(query)
-        return super(Annotation, self).find(annotationQuery, **kwargs)
-
-    def findOne(self, query=None, **kwargs):
-        annotationQuery = self._findQueryFilter(query)
-        return super(Annotation, self).findOne(annotationQuery, **kwargs)
-
     def filter(self, annotation, user=None, additionalKeys=None):
         output = {
             '_id': annotation['_id'],
             '_modelType': 'annotation',
-            'studyId': annotation['meta']['studyId'],
+            'studyId': annotation['studyId'],
             'image': Image().filterSummary(
-                Image().load(annotation['meta']['imageId'], force=True, exc=True),
+                Image().load(annotation['imageId'], force=True, exc=True),
                 user),
             'user': User().filterSummary(
-                user=User().load(annotation['meta']['userId'], force=True, exc=True),
+                user=User().load(annotation['userId'], force=True, exc=True),
                 accessorUser=user),
             'state': Annotation().getState(annotation)
         }
@@ -158,11 +143,11 @@ class Annotation(Item):
                         if not isinstance(featureValue, list) else
                         any(featureValue)
                     for featureId, featureValue in
-                    six.viewitems(annotation['meta']['annotations'])
+                    six.viewitems(annotation['annotations'])
                 },
-                'status': annotation['meta']['status'],
-                'startTime': annotation['meta']['startTime'],
-                'stopTime': annotation['meta']['startTime'],
+                'status': annotation['status'],
+                'startTime': annotation['startTime'],
+                'stopTime': annotation['startTime'],
             })
 
         return output
@@ -170,82 +155,72 @@ class Annotation(Item):
     def filterSummary(self, annotation, user=None):
         return {
             '_id': annotation['_id'],
-            'name': annotation['name'],
-            'studyId': annotation['meta']['studyId'],
-            'userId': annotation['meta']['userId'],
-            'imageId': annotation['meta']['imageId'],
+            'studyId': annotation['studyId'],
+            'userId': annotation['userId'],
+            'imageId': annotation['imageId'],
             'state': self.getState(annotation)
         }
 
     def validate(self, doc):  # noqa - C901
-        # If annotation is fully created
-        if doc.get('meta') and 'studyId' in doc['meta']:
-            for field in ['studyId', 'userId', 'imageId']:
-                if not isinstance(doc['meta'].get(field), ObjectId):
-                    raise ValidationException(
-                        'Annotation field "%s" must be an ObjectId' % field)
+        for field in ['studyId', 'userId', 'imageId']:
+            if not isinstance(doc.get(field), ObjectId):
+                raise ValidationException('Annotation field "%s" must be an ObjectId' % field)
 
-            study = Study().load(doc['meta']['studyId'], force=True, exc=False)
-            if not study:
+        study = Study().load(doc['studyId'], force=True, exc=False)
+        if not study:
+            raise ValidationException(
+                'Annotation field "studyId" must reference an existing Study.')
+
+        # If annotation is complete
+        if doc.get('stopTime'):
+            if not isinstance(doc.get('status'), six.string_types):
+                raise ValidationException('Annotation field "status" must be a string.')
+            for field in ['startTime', 'stopTime']:
+                if not isinstance(doc.get(field), datetime.datetime):
+                    raise ValidationException('Annotation field "%s" must be a datetime.' % field)
+
+            if not isinstance(doc.get('annotations'), dict):
                 raise ValidationException(
-                    'Annotation field "studyId" must reference an existing '
-                    'Study.')
-
-            # If annotation is complete
-            if doc['meta'].get('stopTime'):
-                if not isinstance(doc['meta'].get('status'), six.string_types):
-                    raise ValidationException(
-                        'Annotation field "status" must be a string.')
-                for field in ['startTime', 'stopTime']:
-                    if not isinstance(doc['meta'].get(field),
-                                      datetime.datetime):
+                    'Annotation field "annotations" must be a mapping (dict).')
+            featureset = Study().getFeatureset(study)
+            featuresetGlobalFeatures = {
+                feature['id']: feature
+                for feature in
+                featureset['globalFeatures']
+            }
+            featuresetLocalFeatures = {
+                feature['id']: feature
+                for feature in
+                featureset['localFeatures']
+            }
+            if featuresetLocalFeatures:
+                image = Image().load(doc['imageId'], force=True)
+                superpixels = Image().superpixelsData(image)
+                maxSuperpixel = superpixels.max()
+            else:
+                maxSuperpixel = None
+            for featureId, featureValue in six.viewitems(doc['annotations']):
+                if featureId in featuresetGlobalFeatures:
+                    featureOptions = set(
+                        option['id']
+                        for option in
+                        featuresetGlobalFeatures[featureId]['options'])
+                    if featureValue not in featureOptions:
                         raise ValidationException(
-                            'Annotation field "%s" must be a datetime.' % field)
-
-                if not isinstance(doc['meta'].get('annotations'), dict):
-                    raise ValidationException(
-                        'Annotation field "annotations" must be a mapping '
-                        '(dict).')
-                featureset = Study().getFeatureset(study)
-                featuresetGlobalFeatures = {
-                    feature['id']: feature
-                    for feature in
-                    featureset['globalFeatures']
-                }
-                featuresetLocalFeatures = {
-                    feature['id']: feature
-                    for feature in
-                    featureset['localFeatures']
-                }
-                if featuresetLocalFeatures:
-                    image = Image().load(doc['meta']['imageId'], force=True)
-                    superpixels = Image().superpixelsData(image)
-                    maxSuperpixel = superpixels.max()
+                            'Annotation feature "%s" has invalid value "%s".' %
+                            (featureId, featureValue))
+                elif featureId in featuresetLocalFeatures:
+                    if not (
+                        isinstance(featureValue, list) and
+                        len(featureValue) == maxSuperpixel + 1 and
+                        all(superpixelValue in [0.0, 0.5, 1.0]
+                            for superpixelValue in featureValue)
+                    ):
+                        raise ValidationException(
+                            'Annotation feature "%s" has invalid value "%s".' %
+                            (featureId, featureValue))
                 else:
-                    maxSuperpixel = None
-                for featureId, featureValue in six.viewitems(
-                        doc['meta']['annotations']):
-                    if featureId in featuresetGlobalFeatures:
-                        featureOptions = set(
-                            option['id']
-                            for option in
-                            featuresetGlobalFeatures[featureId]['options'])
-                        if featureValue not in featureOptions:
-                            raise ValidationException(
-                                'Annotation feature "%s" has invalid '
-                                'value "%s".' % (featureId, featureValue))
-                    elif featureId in featuresetLocalFeatures:
-                        if not (
-                            isinstance(featureValue, list) and
-                            len(featureValue) == maxSuperpixel + 1 and
-                            all(superpixelValue in [0.0, 0.5, 1.0]
-                                for superpixelValue in featureValue)
-                        ):
-                            raise ValidationException(
-                                'Annotation feature "%s" has invalid '
-                                'value "%s".' % (featureId, featureValue))
-                    else:
-                        raise ValidationException(
-                            'Annotation has invalid feature "%s".' % featureId)
+                    raise ValidationException(
+                        'Annotation has invalid feature "%s".' % featureId)
 
         return super(Annotation, self).validate(doc)
