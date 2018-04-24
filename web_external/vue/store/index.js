b666e9b923ddd6231361baa467ffde51901ec605
Max Smolens <max.smolens@kitware.com>
2018-04-24 12:43:23 -0400

    Add segmentation review tool
    Add a segmentation review tool to quickly approve or reject
    segmentations for images in the specified dataset.

--- web_external/vue/store/index.js
+++ web_external/vue/store/index.js
@@ -7,12 +7,14 @@ import Vuex from 'vuex';
 
 import annotate from '../components/AnnotationTool/AnnotationToolStore';
 import datasetReview from '../components/DatasetReview/DatasetReviewStore';
+import segmentationReview from '../components/SegmentationReview/SegmentationReviewStore';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         annotate,
-        datasetReview
+        datasetReview,
+        segmentationReview
     }
 });
