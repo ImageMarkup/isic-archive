8b56630c772c38b07d7d1576e3babfb1ca166bb7
Brian Helba <brian.helba@kitware.com>
2018-06-18 12:50:40 -0400

    Add a migration script for metadata files

---
+++ scripts/migrateMetadataFiles.py
@@ -0,0 +1,25 @@
+from girder.utility import server as girder_server
+girder_server.configureServer()
+
+from girder.plugins.isic_archive.models.dataset import Dataset  # noqa: E402
+from girder.models.file import File  # noqa: E402
+from girder.models.folder import Folder  # noqa: E402
+from girder.models.item import Item  # noqa: E402
+
+
+for dataset in Dataset().find():
+    print(dataset['name'])
+    for registration in dataset['metadataFiles']:
+        metadataFile = File().load(registration['fileId'], force=True)
+        item = Item().load(metadataFile['itemId'], force=True)
+        folder = Folder().load(item['folderId'], force=True)
+
+        print(' ', metadataFile['name'])
+
+        del metadataFile['itemId']
+        metadataFile['attachedToType'] = ['dataset', 'isic_archive']
+        metadataFile = File().save(metadataFile)
+
+        File().propagateSizeChange(item, -metadataFile['size'])
+        Item().remove(item)
+        Folder().remove(folder)
